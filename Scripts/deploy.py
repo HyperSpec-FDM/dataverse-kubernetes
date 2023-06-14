fc1ce55aea48067888347ff3cb2aec2533135e65
Tim Häußermann <timi.haeussermann@googlemail.com>
2023-06-14 13:18:45 +0200

    update to dataverse v5.13 working

--- Scripts/deploy.py
+++ Scripts/deploy.py
@@ -13,22 +13,22 @@ os.chdir("..")
 # deploy secrets and dataverse enviroment
 os.system("kubectl apply -f prod-skel\secrets")
 os.system("kubectl apply -k prod-skel\envs\env1")
+os.system("kubectl create -f k8s/dataverse/jobs/bootstrap.yaml")
 
-
-# check if dataverse is running
-while status != 200:
-    try:
-        resp = requests.get(url)
-        status = resp.status_code
-    except requests.exceptions.RequestException as e:
-        print("Waiting for Dataverse to start...")
-        time.sleep(10)
-
-# create bootstrap job configure dataverse
-try:
-    os.system("kubectl create -f k8s/dataverse/jobs/bootstrap.yaml")
-except:
-    print("bootstrap already running")
-
-print("Deployed Dataverse on Kubernetes")
+# # check if dataverse is running
+# while status != 200:
+#     try:
+#         resp = requests.get(url)
+#         status = resp.status_code
+#     except requests.exceptions.RequestException as e:
+#         print("Waiting for Dataverse to start...")
+#         time.sleep(10)
+#
+# # create bootstrap job configure dataverse
+# try:
+#     os.system("kubectl create -f k8s/dataverse/jobs/bootstrap.yaml")
+# except:
+#     print("bootstrap already running")
+#
+# print("Deployed Dataverse on Kubernetes")
 
