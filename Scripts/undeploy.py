f1cb8d36e7fab37ce1a261a4195ef88fdf4b9f19
Tim Häußermann <timi.haeussermann@googlemail.com>
2023-08-16 11:58:00 +0200

    added config-job to postboot script

--- Scripts/undeploy.py
+++ Scripts/undeploy.py
@@ -6,6 +6,7 @@ os.chdir("..")
 
 
 os.system("kubectl delete -f k8s/dataverse/jobs/bootstrap.yaml")
+os.system("kubectl delete -f k8s/dataverse/jobs/configure.yaml")
 os.system("kubectl delete -k prod-skel\envs\env1")
 os.system("kubectl delete -f prod/secrets.yaml")
 os.system("docker container prune -f")
