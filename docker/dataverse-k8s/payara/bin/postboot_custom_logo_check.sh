295d46f9ab15cd0fda2ba32472ecb3416ce4bcce
tim <timi.haeussermann@googlemail.com>
2023-09-28 11:40:35 +0200

    multi node cluster

--- docker/dataverse-k8s/payara/bin/postboot_custom_logo_check.sh
+++ docker/dataverse-k8s/payara/bin/postboot_custom_logo_check.sh
@@ -7,22 +7,17 @@ target_path="/opt/payara/appserver/glassfish/domains/domain1/applications/datave
 # Check if the source directory exists
 if [ -d "$source_path" ]; then
     # Find the latest image file in the source directory
-    latest_image_file=$(find "$source_path" -type f -exec file --mime {} \; | grep -i 'image' | cut -d: -f1 | xargs ls -t | head -n 1)
+    latest_image_file=$(find "$source_path" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.bmp" -o -iname "*.svg" \) -exec stat -c "%Y %n" {} \; | sort -nr | head -n 1 | awk '{print $2}')
 
     # Check if any image file was found
     if [ -n "$latest_image_file" ]; then
-        # Check if the file is one of the recognized image formats (jpg, jpeg, png, gif, bmp, svg)
-        if [[ "$latest_image_file" =~ \.(jpg|jpeg|png|gif|bmp|svg)$ ]]; then
-            # Copy the image file to the target directory
-            cp "$latest_image_file" "$target_path"
+        # Copy the image file to the target directory
+        cp "$latest_image_file" "$target_path"
 
-            # Perform the curl operation
-            curl -X PUT -d "$latest_image_file" http://localhost:8080/api/admin/settings/:LogoCustomizationFile
+        # Perform the curl operation
+        curl -X PUT -d "$latest_image_file" http://localhost:8080/api/admin/settings/LogoCustomizationFile
 
-            echo "Latest image file '$latest_image_file' copied and curl operation performed."
-        else
-            echo "The latest file '$latest_image_file' is not a recognized image file format."
-        fi
+        echo "Latest image file '$latest_image_file' copied and curl operation performed."
     else
         echo "No image files found in directory $source_path."
     fi
