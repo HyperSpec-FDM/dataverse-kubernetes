6466da9abc0bd6c7b9948a5735c171d5ea4247b3
Tim Häußermann <timi.haeussermann@googlemail.com>
2023-11-15 09:41:35 +0100

    updated dataverse 6.0 and solr 9.3.0

--- Scripts/deploy.py
+++ Scripts/deploy.py
@@ -10,7 +10,8 @@ os.system("kubectl create namespace dv-test")
 # os.system('kubectl get secret registry-auth -n docker-registry -o yaml | sed s/"namespace: docker-registry"/"namespace: dv-test"/ | kubectl apply -n dv-test -f -')
 
 # deploy secrets and dataverse enviroment
-os.system("kubectl apply -f prod-skel/secrets")
+# os.system("kubectl apply -f prod-skel/secrets/")
+os.system("kubectl apply -f prod-skel/secrets/secrets.yaml")
 os.system("kubectl apply -k prod-skel/envs/env1")
 # os.system("kubectl apply -f prod-skel/prod/mailcatcher.yaml")
 # os.system("kubectl apply -f k8s/shibboleth/deployment.yaml")
