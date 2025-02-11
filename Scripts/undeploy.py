ccb958105cbe68cbbf9e3f7f4dd849d79b4a0e18
Tim Häußermann <timi.haeussermann@googlemail.com>
2025-02-11 13:46:16 +0100

    update storageclass

--- Scripts/undeploy.py
+++ Scripts/undeploy.py
@@ -6,11 +6,11 @@ os.chdir("..")
 
 # add command to import secret for registry in namespace
 
-# os.system("kubectl delete -f k8s/dataverse/jobs/bootstrap.yaml")
-# os.system("kubectl delete -f k8s/dataverse/jobs/configure.yaml")
-# os.system("kubectl delete -k prod-skel/envs/env1")
-# os.system("kubectl delete -f prod/secrets/secrets.yaml")
-#os.system("kubectl delete -f prod-skel/storageClass.yaml")
+os.system("kubectl delete -f k8s/dataverse/jobs/bootstrap.yaml")
+os.system("kubectl delete -f k8s/dataverse/jobs/configure.yaml")
+os.system("kubectl delete -k prod-skel/envs/env1")
+os.system("kubectl delete -f prod/secrets/secrets.yaml")
+os.system("kubectl delete -f prod-skel/storageClass.yaml")
 # os.system("docker container prune -f")
 print("Undeploy Dataverse")
 
